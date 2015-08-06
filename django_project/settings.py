f5084ed5c44d0615781960f4de7c83bb53f01a7d
Dan <udevelopd@gmail.com>
2015-08-05 19:05:47 -0500

    settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'HOjqPeQWnb',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -107,3 +111,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'PAGE_SIZE': 10
 }
+
+try:
+    from local_settings import *
+except:
+    pass
